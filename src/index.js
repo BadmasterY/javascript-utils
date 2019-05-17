b181fe12a869ca41c81d4867faa9463d9e62f804
Mr <yuzhoujie95@gmail.com>
2019-05-17 16:48:07 +0800

    Update entry file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const { stateMachine } = require('./utils/stateMachine');
+const { ajax } = require('./utils/ajax');
+const { ajaxPromise } = require('./utils/ajaxPromise');
 
 exports.stateMachine = stateMachine;
+exports.ajax = ajax;
+exports.ajaxPromise = ajaxPromise;

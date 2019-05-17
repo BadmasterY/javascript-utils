9eabc4da7fa7101c1dff70dde625a25fbac9c394
Mr <yuzhoujie95@gmail.com>
2019-05-17 13:43:06 +0800

    update stateMachine.test.js

--- tests/stateMachine.test.js
+++ tests/stateMachine.test.js
@@ -12,47 +12,67 @@ let state = new stateMachine({
         onUnStart: () => {},
         onStart: () => {},
         onEnd: () => {},
+        onError: () => {}
     }
 })
 
-describe('stateMachine.next', () => {
-    it(`The current state is 'UnStart'`, () => {
-        assert.equal(state.state, 'UnStart');
+describe('# stateMachine', () => {
+    describe('- stateMachine.next', () => {
+        it(`The current state is 'UnStart'`, () => {
+            assert.equal(state.state, 'UnStart');
+        });
+    
+        it(`The current state is 'Start'`, () => {
+            state.next();
+            assert.equal(state.state, 'Start');
+        });
+    
+        it(`The current state is 'End'`, () => {
+            state.next();
+            assert.equal(state.state, 'End');
+        });
+    
+        it(`The current state is 'UnStart'`, () => {
+            state.next();
+            assert.equal(state.state, 'UnStart');
+        });
+    });
+    
+    describe('- stateMachine.pre', () => {
+        it(`The current state is 'UnStart'`, () => {
+            assert.equal(state.state, 'UnStart');
+        });
+    
+        it(`The current state is 'End'`, () => {
+            state.pre();
+            assert.equal(state.state, 'End');
+        });
+    
+        it(`The current state is 'Start'`, () => {
+            state.pre();
+            assert.equal(state.state, 'Start');
+        });
+    
+        it(`The current state is 'UnStart'`, () => {
+            state.pre();
+            assert.equal(state.state, 'UnStart');
+        });
     });
 
-    it(`The current state is 'Start'`, () => {
-        state.next();
-        assert.equal(state.state, 'Start');
-    });
+    describe('- stateMachine.to', () => {
+        it(`The current state is 'Start'`, () => {
+            state.to('Start')
+            assert.equal(state.state, 'Start');
+        });
 
-    it(`The current state is 'End'`, () => {
-        state.next();
-        assert.equal(state.state, 'End');
-    });
+        it(`The current state is 'Error'`, () => {
+            state.to('Error')
+            assert.equal(state.state, 'Error');
+        });
 
-    it(`The current state is 'UnStart'`, () => {
-        state.next();
-        assert.equal(state.state, 'UnStart');
+        it(`The current state is 'UnStart'`, () => {
+            state.to('UnStart')
+            assert.equal(state.state, 'UnStart');
+        });
     });
 });
-
-describe('stateMachine.pre', () => {
-    it(`The current state is 'UnStart'`, () => {
-        assert.equal(state.state, 'UnStart');
-    });
-
-    it(`The current state is 'End'`, () => {
-        state.pre();
-        assert.equal(state.state, 'End');
-    });
-
-    it(`The current state is 'Start'`, () => {
-        state.pre();
-        assert.equal(state.state, 'Start');
-    });
-
-    it(`The current state is 'UnStart'`, () => {
-        state.pre();
-        assert.equal(state.state, 'UnStart');
-    });
-})

837c3645ad62632037cff40e13569f930e81a2fc
Mr <yuzhoujie95@gmail.com>
2019-05-28 10:05:32 +0800

    update code, check bug

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { differenceSet } = require('./utils/differenceSet');
 const { intersection } = require('./utils/intersection');
 const { complementSet } = require('./utils/complementSet');
 const { union } = require('./utils/union');
+const { dataType } = require('./utils/dataType');
 
 exports.stateMachine = stateMachine; // 状态机
 exports.ajax = ajax; // 简单异步ajax
@@ -13,3 +14,4 @@ exports.differenceSet = differenceSet; // 差集
 exports.intersection = intersection; // 交集
 exports.complementSet = complementSet; // 补集
 exports.union = union; // 并集
+exports.dataType = dataType; // 判断数据类型

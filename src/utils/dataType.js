9f099de8ecb26739a8f7f49f968ec3c9f9afc1e0
Mr <yuzhoujie95@gmail.com>
2019-05-30 09:42:52 +0800

    use regexp matching

--- src/utils/dataType.js
+++ src/utils/dataType.js
@@ -3,30 +3,11 @@
  * dataType - v0.0.1 (2019-05-28 09:35:10)
  * Author: Mr. 854007673@qq.com
  */
-const { message } = require('./message');
 
 exports.dataType = data => {
-    switch (Object.prototype.toString.call(data)) {
-        case '[object Undefined]':
-            return undefined;
-        case '[object Null]':
-            return null;
-        case '[object String]':
-            return String;
-        case '[object Object]':
-            return Object;
-        case '[object Number]':
-            return Number;
-        case '[object Array]':
-            return Array;
-        case '[object Function]':
-            return Function;
-        case '[object Boolean]':
-            return Boolean;
-        case '[object Symbol]':
-            return Symbol;
-        default:
-            message('warn', `This type ${Object.prototype.toString.call(data)} is not found.`);
-            break;
-    }
+
+    let typeStr = Object.prototype.toString.call(data);
+    let regexp = new RegExp('(\\w+)(?=])');
+
+    return typeStr.match(regexp)[0];
 }

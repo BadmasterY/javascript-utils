d85f713e21bb6aaa82d8100913d9a1858d38bc51
Mr <yuzhoujie95@gmail.com>
2019-05-30 09:46:49 +0800

    update code

--- src/utils/dataType.js
+++ src/utils/dataType.js
@@ -1,13 +1,14 @@
 /*!
  * JavaScript data type check
- * dataType - v0.0.1 (2019-05-28 09:35:10)
+ * dataType - v0.0.2 (2019-05-30 09:45:57)
  * Author: Mr. 854007673@qq.com
+ * use regexp matching
  */
 
 exports.dataType = data => {
 
     let typeStr = Object.prototype.toString.call(data);
-    let regexp = new RegExp('(\\w+)(?=])');
+    let regexp = new RegExp('(\\w+)(?=])', 'g');
 
     return typeStr.match(regexp)[0];
 }
